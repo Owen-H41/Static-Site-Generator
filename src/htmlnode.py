010a633237756784fd701bb11d79dc84504f6652
owen.h <owen.hallett@outlook.com>
2026-06-17 16:56:13 +0100

    Created HTMLNode and relevant test File

---
+++ src/htmlnode.py
@@ -0,0 +1,21 @@
+class HTMLNode:
+    def __init__(self, tag=None, value=None, children=None, props=None):
+        self.tag = tag
+        self.value = value
+        self.children = children
+        self.props = props
+    
+    def to_html(self):
+        raise NotImplementedError
+    
+    def props_to_html(self):
+        if self.props == None:
+            return ""
+        props_list = []
+        for prop in self.props:
+            props_list.append(f" {prop}={self.props[prop]}")
+        prop_string = "".join(props_list)
+        return prop_string
+
+    def __repr__(self):
+        return f"HTMLNode(Tag={self.tag}, Value={self.value}, Children={self.children}, Props={self.props_to_html()})"
---
+++ src/test_htmlnode.py
@@ -0,0 +1,26 @@
+import unittest
+from htmlnode import HTMLNode
+
+class TestHTMLNode(unittest.TestCase):
+    def test_props_to_html(self):
+        node = HTMLNode("test", "dummy", props={"key1":"value1", "key2":"value2", "key3":"value3"})
+        self.assertEqual(node.props_to_html(), ' key1=value1 key2=value2 key3=value3')
+    
+    def test_empty_props_to_html(self):
+        node = HTMLNode("test", "dummy")
+        self.assertEqual(node.props_to_html(), "")
+    
+    def test_empty_children(self):
+        node = HTMLNode("test", "dummy")
+        self.assertEqual(node.children, None)
+
+    def test_empty_props(self):
+        node = HTMLNode("test", "dummy")
+        self.assertEqual(node.props, None)
+    
+    def test___repr__(self):
+        node = HTMLNode("test", "dummy", props={"key1":"value1", "key2":"value2", "key3":"value3"})
+        self.assertEqual(node.__repr__(), 'HTMLNode(Tag=test, Value=dummy, Children=None, Props= key1=value1 key2=value2 key3=value3)')
+
+if __name__ == "__main__":
+    unittest.main()
